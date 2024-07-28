89b8996d8cb1ea8e330e684a537237a8e5c2b757
Rozina-Gebrehiwet <166912787+Rozinagebrehiwet@users.noreply.github.com>
2024-07-28 19:55:26 +0300

    Update home.js

--- src/pages/home.js
+++ src/pages/home.js
@@ -11,68 +11,68 @@ import MyCarousel from './components/my-carousel';
 
 const products = [
   {
-    "id" : 1,
-    "href" : "img",
-   "imageAlt" : "products",
-   "imageSrc" : require('./assets/a5dcb73787322a8244171b9eea44b9bd-1.jpg'),
-   "name" : "Especial Burger",
-  "price" : 269.99
+    "id": 1,
+    "href": "img",
+    "imageAlt": "products",
+    "imageSrc": require('./assets/a5dcb73787322a8244171b9eea44b9bd-1.jpg'),
+    "name": "Especial Burger",
+    "price": 269.99
   },
   {
-    "id" : 1,
-    "href" : "img",
-   "imageAlt" : "products",
-   "imageSrc" : require('./assets/a5dcb73787322a8244171b9eea44b9bd-1.jpg'),
-   "name" : "Especial Burger",
-  "price" : 269.99
+    "id": 1,
+    "href": "img",
+    "imageAlt": "products",
+    "imageSrc": require('./assets/a5dcb73787322a8244171b9eea44b9bd-1.jpg'),
+    "name": "Especial Burger",
+    "price": 269.99
   },
   {
-    "id" : 1,
-    "href" : "img",
-   "imageAlt" : "products",
-   "imageSrc" : require('./assets/a5dcb73787322a8244171b9eea44b9bd-1.jpg'),
-   "name" : "Especial Burger",
-  "price" : 269.99
+    "id": 1,
+    "href": "img",
+    "imageAlt": "products",
+    "imageSrc": require('./assets/a5dcb73787322a8244171b9eea44b9bd-1.jpg'),
+    "name": "Especial Burger",
+    "price": 269.99
   },
   {
-    "id" : 1,
-    "href" : "img",
-   "imageAlt" : "products",
-   "imageSrc" : require('./assets/a5dcb73787322a8244171b9eea44b9bd-1.jpg'),
-   "name" : "Especial Burger",
-  "price" : 269.99
+    "id": 1,
+    "href": "img",
+    "imageAlt": "products",
+    "imageSrc": require('./assets/a5dcb73787322a8244171b9eea44b9bd-1.jpg'),
+    "name": "Especial Burger",
+    "price": 269.99
   },
   {
-    "id" : 1,
-    "href" : "img",
-   "imageAlt" : "products",
-   "imageSrc" : require('./assets/a5dcb73787322a8244171b9eea44b9bd-1.jpg'),
-   "name" : "Especial Burger",
-  "price" : 269.99
+    "id": 1,
+    "href": "img",
+    "imageAlt": "products",
+    "imageSrc": require('./assets/a5dcb73787322a8244171b9eea44b9bd-1.jpg'),
+    "name": "Especial Burger",
+    "price": 269.99
   },
   {
-    "id" : 1,
-    "href" : "img",
-   "imageAlt" : "products",
-   "imageSrc" : require('./assets/a5dcb73787322a8244171b9eea44b9bd-1.jpg'),
-   "name" : "Especial Burger",
-  "price" : 269.99
+    "id": 1,
+    "href": "img",
+    "imageAlt": "products",
+    "imageSrc": require('./assets/a5dcb73787322a8244171b9eea44b9bd-1.jpg'),
+    "name": "Especial Burger",
+    "price": 269.99
   },
   {
-    "id" : 1,
-    "href" : "img",
-   "imageAlt" : "products",
-   "imageSrc" : require('./assets/a5dcb73787322a8244171b9eea44b9bd-1.jpg'),
-   "name" : "Especial Burger",
-  "price" : 269.99
+    "id": 1,
+    "href": "img",
+    "imageAlt": "products",
+    "imageSrc": require('./assets/a5dcb73787322a8244171b9eea44b9bd-1.jpg'),
+    "name": "Especial Burger",
+    "price": 269.99
   },
   {
-    "id" : 1,
-    "href" : "img",
-   "imageAlt" : "products",
-   "imageSrc" : require('./assets/a5dcb73787322a8244171b9eea44b9bd-1.jpg'),
-   "name" : "Especial Burger",
-  "price" : 269.99
+    "id": 1,
+    "href": "img",
+    "imageAlt": "products",
+    "imageSrc": require('./assets/a5dcb73787322a8244171b9eea44b9bd-1.jpg'),
+    "name": "Especial Burger",
+    "price": 269.99
   },
 ]
 
@@ -80,78 +80,82 @@ function Home() {
 
 
   return (
-        <div className='relative bg-custom_background z-1'>
+    <div className='relative bg-custom_background dark:bg-slate-900 z-1'>
 
-          <div className="w-full h-screen mx-auto bg-pinkSoft">
-          <Navbar />
-          <div className='h-[490px] w-[100%]'>
+      <div className="w-full h-screen mx-auto bg-pinkSoft">
+        <Navbar />
+        <div className='h-[490px] w-[100%]'>
 
           <MyCarousel />
-          </div>
-          
-           </div>
+        </div>
 
-
-          <Fade up> 
-          <div className="welcome-container relative w-[100%]   h-[1000px] md:h-[500px]  overflow-hidden my-[50px] md:my-[80px] mx-auto">
-              <div className='bg-custom_primary_25 w-[95%]  h-[500px]  mx-[2%]  lg:w-[70%]   overflow-hidden rounded-tr-[50px] rounded-bl-[50px] float-left'>
-                <div className='welcome-content md:w-[50%] lg:w-[75%]  mt-[36px] ml-[30px] md:ml-[5px] lg:ml-[30px]'>
-                   <Fade up> 
-                  <h2 className='welcome-header text-custom_primary text-[50px] text-left'>
-                    Welcome
-                  </h2>
-                    </Fade>
-                    <Fade up> 
-                  <p className='text-left text-[18px] font-normal px-[5px]'>
-                  <span className='pl-8'></span>Welcome
-    There are many variations of passages of Lorem Ipsum available, 
-but the majority have suffered alteration in some form, by injected 
-humour, or randomised words which don't look even slightly believable.There are many variations of passages of Lorem Ipsum availablethe majority have suffered alteration in some form, by injected or randomised words which don't look even slightl believable.
-</p>   </Fade>
-<div className='reserve-button my-[10px] mx-auto mt-[60px] w-[220px] p-[15px] rounded-[12px] font-[600] text-[20px]'>Reserve table</div>
-                </div>
-              </div>
-
-              <Fade right>
-              <div className="welcome-showcase absolute mx-auto top-[495px] left-2 md:left-[50%] md:top-[0px] lg:left-[58%] lg:top-[20px] w-[98%] md:w-[50%] md:w-[400px] lg:w-[530px] h-[430px]">
-              <Fade right delay={3} duration={1000} ><div className="welcome-showcase-image-1 absolute top-0 left-0 w-[60%] md:w-[80%] h-[380px] rounded-[16px]"></div></Fade>
-              <Fade right delay={7} duration={1000} > <div className="welcome-showcase-image-2  absolute bottom-0 right-2  md:right-0  lg:right-2 w-[60%] md:w-[70%]  h-[380px] rounded-[16px]"></div></Fade>
-              </div>
-              </Fade>
-           </div>
-           </Fade>
-
-
-<Fade up> 
-          <div className="wrapper-container mx-[20px] md:mx-[90px] my-0 z-100"> 
- 
-           <p className='wrapper-title text-[35px]  flex items-center justify-center'><span className='inline-block my-0 mx-[10px] md:mx-[28px] w-[40px] h-[3px] border-t-[1px] border-solid border-black z-1'></span>today’s special<span className='inline-block my-0 mx-[10px] md:mx-[28px] w-[40px] h-[3px] border-t-[1px] border-solid border-black z-1'></span></p>
-           <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
-           <h2 className="sr-only">Products</h2>
-            <div className='relative grid grid-cols-1 gap-x-20 gap-y-20 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8   z-3'>
-              {products.map((item,index) => (       
-                <ProductCard product={item}/>
-              ))}
-              
-            </div>
-            </div>
-            
-            <div className='discover-button my-[10px] mt-[35px] mx-auto w-[180px] p-[7px]  rounded-[50px]'><span className='font-[700]'>Discover More!</span></div>
-           </div> 
-           </Fade>
-           <Fade up> 
-          <div className='contact-container my-[60px] mx-[auto] w-[94%]'>
-           <Contact />
-            </div> 
-            </Fade>
-          <div className='about-container'>
-              <Footer />
-            </div> 
-          
-         
       </div>
-      
-   
+
+
+      <Fade up>
+        <div className="welcome-container relative w-[100%]   h-[1000px] md:h-[500px]  overflow-hidden my-[50px] md:my-[80px] mx-auto">
+          <div className='bg-custom_primary_25 w-[95%]  h-[500px]  mx-[2%]  lg:w-[70%]   overflow-hidden rounded-tr-[50px] rounded-bl-[50px] float-left'>
+            <div className='welcome-content md:w-[50%] lg:w-[75%]  mt-[36px] ml-[30px] md:ml-[5px] lg:ml-[30px]'>
+              <Fade up>
+                <h2 className='welcome-header text-custom_primary text-[50px] text-left my-5 pt-3'>
+                  Welcome
+                </h2>
+              </Fade>
+              <Fade up>
+                <p className='text-left text-[16px]  font-normal px-[5px] text-slate-900 dark:text-slate-200'>
+                  Welcome
+                  There are many variations of passages of Lorem Ipsum available,
+                  but the majority have suffered alteration in some form, by injected
+                  humour, or randomised words which don't look even slightly believable.There are many variations of passages of Lorem Ipsum availablethe majority have suffered alteration in some form, by injected or randomised words which don't look even slightl believable.
+                </p>   </Fade>
+            
+                  <div className='reserve-button my-[10px] mx-auto mt-[60px] w-[220px] p-[15px] rounded-[12px] font-[600] text-[20px] '>Reserve table</div>
+            </div>
+          </div>
+
+          <Fade right>
+            <div className="welcome-showcase absolute mx-auto top-[495px] left-2 md:left-[50%] md:top-[0px] lg:left-[58%] lg:top-[20px] w-[98%] md:w-[50%] md:w-[400px] lg:w-[530px] h-[430px]">
+              <Fade right delay={10} duration={1000} ><div className="welcome-showcase-image-1 absolute top-0 left-0 w-[60%] md:w-[80%] h-[380px] rounded-[16px]"></div></Fade>
+              <Fade right delay={3} duration={1000} > <div className="welcome-showcase-image-2  absolute bottom-0 right-2  md:right-0  lg:right-2 w-[60%] md:w-[70%]  h-[380px] rounded-[16px]"></div></Fade>
+            </div>
+          </Fade>
+        </div>
+      </Fade>
+
+
+      <Fade up>
+        <div className="wrapper-container mx-[20px] md:mx-[90px] my-0 z-100">
+
+          <p className='wrapper-title text-[35px]  flex items-center justify-center dark:text-slate-200'>
+            <span className='inline-block my-0 mx-[10px] md:mx-[28px] w-[40px] h-[3px] border-t-[1px] border-solid border-slate-200 z-1'></span>
+            today’s special
+            <span className='inline-block my-0 mx-[10px] md:mx-[28px] w-[40px] h-[3px] border-t-[1px] border-solid border-slate-200 z-1'></span></p>
+          <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
+            <h2 className="sr-only">Products</h2>
+            <div className='relative grid grid-cols-1 gap-x-20 gap-y-20 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8   z-3'>
+              {products.map((item, index) => (
+                <ProductCard product={item} />
+              ))}
+
+            </div>
+          </div>
+
+          <div className='discover-button my-[10px] mt-[35px] mx-auto w-[180px] p-[7px]  rounded-[50px]'><span className='font-[700] text-slate-900 dark:text-slate-200'>Discover More!</span></div>
+        </div>
+      </Fade>
+      <Fade up>
+        <div className='contact-container my-[60px] mx-[auto] w-[94%]'>
+          <Contact />
+        </div>
+      </Fade>
+      <div className='about-container'>
+        <Footer />
+      </div>
+
+
+    </div>
+
+
   );
- }
+}
 export default Home;
