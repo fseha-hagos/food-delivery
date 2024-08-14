f5473ed0f9413bd37d558f4e34466e5dfd1e8063
fish <fsehahagos23@gmail.com>
2024-08-14 05:19:11 -0300

    mmm

--- src/App.js
+++ src/App.js
@@ -58,24 +58,14 @@ import Loginpage from './views/Loginpage'
 import Dashboard from './views/Dashboard'
 import Navbar from './pages/components/navbar'
 import Footer from './pages/components/footer'
+import AboutUs from './pages/AboutUs';
+import Contact from './pages/Contact';
+import Home from './pages/home';
+import MenuRecipe from './pages/MenuRecipe';
+import OrderOnline from './pages/OrderOnline';
+import RecipeDetail from './pages/RecipeDetail';
+import SignUp from './pages/SignUp';
 
-
-
-// <Router>
-// <AuthProvider>
-//   < Navbar/>
-  
-//   <Routes>
-//    <Route  component={PrivateRoute}  path='/' exact>
-//      <Route component={Dashboard} path="/dashboard" exact />
-//    </Route>
-   
-//     <Route component={Loginpage} path="/login" />
-//     <Route component={Registerpage} path="/register" exact />
-//     <Route component={Homepage} path="/" exact />
-//   </Routes>
-// </AuthProvider>
-// </Router>
 
 
 function App() {
@@ -83,17 +73,37 @@ function App() {
    
      <Router>
      <AuthProvider>
-       < Navbar/>
+      
     
        <Routes>
+                   <Route path='/home' element={ <PrivateRoute> <Home /> </PrivateRoute> }></Route>
+                   <Route path="/"
+                        element={<Home />} />
+                    <Route path="/home"
+                        element={<Home />} />
+                    <Route path="/menu"
+                        element={<MenuRecipe />} />
+                    <Route path="/order-online"
+                        element={<OrderOnline />} />
+                    <Route path="/recipe"
+                        element={<RecipeDetail />} />
+                    <Route path="/about"
+                        element={<AboutUs />} />
+                    <Route path="/login"
+                        element={<SignUp />} />
+                    <Route path="/contact"
+                        element={<Contact />} />
  
+            {/*  
          <Route element={<Loginpage />} path="/login" />
-         <Route path='/dashboard' element={ <PrivateRoute> <Dashboard /> </PrivateRoute> }></Route>
+     
          <Route element={<Registerpage />} path="/register" exact />
          <Route element={<Homepage />} path="/" exact />
-       </Routes>
+       
+*/}
 
-       < Footer/>
+</Routes>
+       
      </AuthProvider>
    </Router>
   )
