5ecaf1f276b59081b0638a5a9d10bac70b0e9f3c
Rozina-Gebrehiwet <166912787+Rozinagebrehiwet@users.noreply.github.com>
2024-08-21 08:24:58 +0300

    Update login.js

--- src/pages/components/login.js
+++ src/pages/components/login.js
@@ -26,10 +26,10 @@ const Login = ({ isOpen, onClose, children }) => {
   if (!isOpen) return null;
 
   return ReactDOM.createPortal(
-    <div className="fixed top-0 left-0 w-full h-full bg-slate-900/50 items-center justify-center z-1000">
+    <div className="fixed top-0 left-0 w-full h-full bg-slate-900/50 items-center justify-center z-50">
      <Zoom out delay={100}>
       <div className='flex items-center justify-center h-screen w-screen'>
-      <div className="bg-amber-600/90 p-3 rounded shadow-lg w-25 max-w-100%" ref={loginRef}>
+      <div className="bg-amber-600/95 p-3 rounded shadow-lg w-25 max-w-100%" ref={loginRef}>
         {children}
       </div>
       </div>
