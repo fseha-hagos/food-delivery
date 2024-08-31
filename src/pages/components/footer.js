7f50c39227a5f8b3b53eabdc71b36e9b8a337848
Rozina-Gebrehiwet <166912787+Rozinagebrehiwet@users.noreply.github.com>
2024-08-31 20:29:19 +0300

    Update footer.js

--- src/pages/components/footer.js
+++ src/pages/components/footer.js
@@ -1,12 +1,11 @@
 import React from "react";
 import logo from "../assets/logo.png";
-import { Slide } from "react-reveal";
+import { Fade } from "react-reveal";
 
 const Footer = () => {
     return (
-      
+      <Fade up>
         <footer className="bg-slate-200 dark:bg-slate-950">
-        <Slide down>
             <div className="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8">
                 <div className="md:flex md:justify-between">
                     <div className="mb-6 md:mb-0">
@@ -27,10 +26,8 @@ const Footer = () => {
                     </div>
                 </div>
             </div>
-            </Slide>
         </footer>
-            
-        
+        </Fade>
     );
 }
 
