12ab661272bcdfa64f304fb6dbdbb99eb2418bfd
Rozina-Gebrehiwet <166912787+Rozinagebrehiwet@users.noreply.github.com>
2024-09-01 16:40:11 +0300

    Update cart.js

--- src/pages/cart.js
+++ src/pages/cart.js
@@ -72,57 +72,60 @@ function Cart() {
                 <Fade up delay={300}>
                     <h1 className="text-2xl font-bold mt-3 mb-2 text-amber-500" > Shopping Cart</h1>
                     <div className=' text-slate-200 mb-5 '> your cart has items </div>
-                    <div className='block md:flex '>
+                </Fade>
+                <div className='block md:flex '>
+                    <Fade left delay={400}>
                         <ul className="list-none bg-slate-800 w-3/4  ">
                             {cartItems.map(item => (
                                 <li key={item.id}>
-                                    <div className="flex  mt-5  text-slate-200  ">
-                                        <img src={item.image} alt={item.name} className="w-1/3 h-12" />
-                                        <div className='mx-3'>
-                                            <h2 className="text-lg mb-3 " >{item.name} </h2>
-                                            <p className='pr-5'>{item.description}</p>
+                                    <Fade up delay={600}>
+                                        <div className="flex  mt-5  text-slate-200  ">
+                                            <img src={item.image} alt={item.name} className="w-1/3 h-12" />
+                                            <div className='mx-3'>
+                                                <h2 className="text-lg mb-3 " >{item.name} </h2>
+                                                <p className='pr-5'>{item.description}</p>
+                                            </div>
+                                            <div className='mx-3'>
+                                                <div>Qty:</div>
+                                                <hr />
+                                                <input type="number" value={item.quantity} onChange={e => handleQuantityChange(item.id, e.target.value)}
+                                                    className="w-12 pl-2 text-sm bg-transparent"
+                                                />
+                                            </div>
+                                            <div className='mx-3'>
+                                                <div>{item.price} birr</div>
+                                                <div className='text-sm text-slate-400 mb-2'>price per one</div>
+                                                <div>{calculateItemTotal(item).toFixed(2)} birr</div>
+                                                <div className='text-sm text-slate-400 mb-5'>total price</div>
+                                                <button onClick={() => handleRemoveItem(item.id)}
+                                                    className="bg-violet-700 hover:bg-rose-700  text-slate-100 font-bold mb-5 py-2 px-4 rounded"
+                                                >
+                                                    Remove
+                                                </button>
+                                            </div>
                                         </div>
-                                        <div className='mx-3'>
-                                            <div>Qty:</div>
-                                            <hr />
-                                            <input type="number" value={item.quantity}
-                                                onChange={e => handleQuantityChange(item.id, e.target.value)}
-                                                className="w-12 pl-2 text-sm bg-transparent"
-                                            />
-
-
-                                        </div>
-                                        <div className='mx-3'>
-                                            <div>{item.price} birr</div>
-                                            <div className='text-sm text-slate-400 mb-2'>price per one</div>
-                                            <div>{calculateItemTotal(item).toFixed(2)} birr</div>
-                                            <div className='text-sm text-slate-400 mb-5'>total price</div>
-                                            <button
-                                                onClick={() => handleRemoveItem(item.id)}
-                                                className="bg-violet-700 hover:bg-rose-700  text-slate-100 font-bold mb-5 py-2 px-4 rounded"
-                                            >
-                                                Remove
-                                            </button>
-                                        </div>
-                                    </div>
+                                    </Fade>
                                     <hr />
                                 </li>
                             ))}
-
                         </ul>
+                    </Fade>
+                    <Fade right delay={500}>
                         <div className="w-1/3 h-1/2  mx-1 p-5 bg-slate-800 text-slate-200" >
-
-                            <h2 className="text-lg mb-3 ">Cart Summary</h2>
-                            <p>Subtotal:<span className='float-right' >{calculateSubtotal().toFixed(2)} birr</span></p>
-                            <p>Tax:<span className='float-right' >{calculateTax().toFixed(2)} birr</span></p>
-                            <hr />
-                            <p>Total:<span className='float-right' >{calculateTotal().toFixed(2)} birr</span></p>
-                            <button className="bg-amber-500 float-right hover:bg-orange-700 text-white font-bold px-5 py-2 my-3 rounded-full">
-                                Checkout
-                            </button>
+                            <Fade up delay={600}>
+                                <h2 className="text-lg mb-3 ">Cart Summary</h2>
+                                <p>Subtotal:<span className='float-right' >{calculateSubtotal().toFixed(2)} birr</span></p>
+                                <p>Tax:<span className='float-right' >{calculateTax().toFixed(2)} birr</span></p>
+                                <hr />
+                                <p>Total:<span className='float-right' >{calculateTotal().toFixed(2)} birr</span></p>
+                                <button className="bg-amber-500 float-right hover:bg-orange-700 text-white font-bold px-5 py-2 my-3 rounded-full">
+                                    Checkout
+                                </button>
+                            </Fade>
                         </div>
-                    </div>
-                </Fade>
+                    </Fade>
+
+                </div>
             </div >
             <Footer />
         </div >
