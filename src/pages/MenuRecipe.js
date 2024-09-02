c48bcae77f32833e05b3f18fdd14afb538e2049e
Rozina-Gebrehiwet <166912787+Rozinagebrehiwet@users.noreply.github.com>
2024-09-02 08:29:20 +0300

    Update MenuRecipe.js

--- src/pages/MenuRecipe.js
+++ src/pages/MenuRecipe.js
@@ -1,17 +1,17 @@
 import '@fontsource/lily-script-one';
 import './styles/home.css'
 import Navbar from "./components/navbar";
-import ProductCard from "./components/product-card";
-import Contact from './components/contact';
 import Footer from './components/footer';
 import RecipeRProductCard from './components/recipeRProductCard';
 import { IconContext } from 'react-icons';
 import { CiCircleChevLeft } from "react-icons/ci";
 import { CiCircleChevRight } from "react-icons/ci";
-import { Fade, Slide, Bounce } from 'react-reveal';
+import { Fade } from 'react-reveal';
 import { Link } from 'react-router-dom';
 import { useEffect, useState } from 'react';
 import { BACKEND_BASE_URL } from '../context/constants';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faChevronLeft, faChevronRight } from '@fortawesome/free-solid-svg-icons';
 
 const menuItems = [
   { text: 'All', className: 'menu-btn-all' },
@@ -27,7 +27,7 @@ const menuItems = [
   { text: 'Alcoholic Beverages', className: 'menu-btn-alcoholic-beverages' }
 ];
 
-const MenuList = ({catagoryLists}) => {
+const MenuList = ({ catagoryLists }) => {
   return (
     <div className='menu-list'>
       {catagoryLists.map((item, index) => (
@@ -47,44 +47,44 @@ function MenuRecipe() {
   const [latestItems, setLatestItems] = useState([]);
 
   useEffect(
-    ()=>{
+    () => {
       getCatagoryItems()
       getLatestItems()
     }
-  , [])
+    , [])
 
 
   const getCatagoryItems = async () => {
-    try{
+    try {
       let response = fetch(
-        BACKEND_BASE_URL+"/catagory");
-            
-        // FetchRes is the promise to resolve
-        // it by using.then() method
-        response.then(res =>
-            res.json()).then(d => {
-              setCatagoryLists(d)
-              console.log("catgory loaded succesfully");
-              console.log(d)
-            });
-    }catch{
+        BACKEND_BASE_URL + "/catagory");
+
+      // FetchRes is the promise to resolve
+      // it by using.then() method
+      response.then(res =>
+        res.json()).then(d => {
+          setCatagoryLists(d)
+          console.log("catgory loaded succesfully");
+          console.log(d)
+        });
+    } catch {
       console.log("(MenuRecipe.js) === there was a server issue");
     }
   }
- 
+
   const getLatestItems = async () => {
-    try{
-      let response = fetch(BACKEND_BASE_URL+"/menu");
-            
-        // FetchRes is the promise to resolve
-        // it by using.then() method
-        response.then(res =>
-            res.json()).then(d => {
-              setLatestItems(d)
-              console.log("menus loaded succesfully");
-              console.log(d)
-            });
-    }catch{
+    try {
+      let response = fetch(BACKEND_BASE_URL + "/menu");
+
+      // FetchRes is the promise to resolve
+      // it by using.then() method
+      response.then(res =>
+        res.json()).then(d => {
+          setLatestItems(d)
+          console.log("menus loaded succesfully");
+          console.log(d)
+        });
+    } catch {
       console.log("there was a server issue");
     }
   }
@@ -122,11 +122,13 @@ function MenuRecipe() {
             <div className='h-auto mx-5'> <img src={require("./assets/logo.png")} alt={"Carlie Anglemire"} style={{ width: "250px", height: "250px" }} /> </div>
           </Fade>
         </div>
+        <Fade up>
         <h2 className='flex lg:w-1/2 text-amber-500 mb-5'>
           <hr className=' border-white w-24 mx-2' />
           Our menu
           <hr className='menu-hr border-white w-24 mx-2' />
         </h2>
+        </Fade>
         <Fade up>
         <MenuList catagoryLists = {catagoryLists}/>
         </Fade>
@@ -141,31 +143,15 @@ function MenuRecipe() {
 
 
       </div>
-      <div className='menu-botm-nav'>
-        <div className=' button nav-button-icon'><IconContext.Provider value={{ color: 'white', size: '30px', className: 'menu-icon' }} >
-          <div>
-            <CiCircleChevLeft />
-          </div>
-        </IconContext.Provider>
+      <div className='flex text-xl  text-slate-200 items-center justify-center  my-[5%] mx-[5%] '>
 
-          <span className='text-slate-900 dark:text-slate-200 text-xs'>Prev</span>
-        </div>
-        <div className='button nav-button-icon'>
-           <span className='text-slate-900 dark:text-slate-200 text-xs'>Next</span>
-           <IconContext.Provider value={{ color: 'white', size: '30px', className: 'menu-icon' }} >
-          <div >
-            <CiCircleChevRight />
+        <div className=' text-left w-[100%] '><a href='/prev' className='text-slate-200 no-underline cursor-pointer'>prev <FontAwesomeIcon icon={faChevronLeft} size='x'/></a></div>          
+        <div className=' text-right w-[100%] '><a href='/next' className='text-slate-200  no-underline cursor-pointer'><FontAwesomeIcon icon={faChevronRight} size='x' /> next</a> </div>         
+         
           </div>
-        </IconContext.Provider>
-        </div>
-      </div>
-
-      <div className='about-container'>
         <Footer />
-      </div>
 
-
-    </div>
+</div >
 
 
   );
